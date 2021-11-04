ddd502b5ec8538178a1e473b025fe89368aa035c
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2021-11-04 01:56:33 +0100

    archive.zip

--- credit_card_pipeline/scripts/prepare.py
+++ credit_card_pipeline/scripts/prepare.py
@@ -2,14 +2,19 @@
 
 import yaml
 import pandas as pd
+import zipfile
+import os
 
 
 def main():
     # Read parameters
     params = yaml.safe_load(open('params.yaml'))['prepare']
     data_path = params['data_path']
+    archive_path = params['archive_path']
 
-    # Read data (in this case there isn't any special preprocessing)
+    # Read data & download if missing (in this case there isn't any special preprocessing)
+    if not os.path.exists(data_path):
+        zipfile.ZipFile(archive_path, 'r').extractall('data')
     data = pd.read_csv(data_path)
 
     data.to_pickle('data/prepared.pkl')
